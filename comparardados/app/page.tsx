88be8d30fd463792aac8f7d2f73bafe1433638dd
Gabriel Matos <120828640+Gabriel-Matos1@users.noreply.github.com>
2024-02-26 09:10:04 -0300

    Update page.tsx

--- comparardados/app/page.tsx
+++ comparardados/app/page.tsx
@@ -1,150 +1,150 @@
-'use client'
+'use client';
 
-import DateFilter from "@/components/filtroData";
-import FiltroRadio from "@/components/filtroarq";
-import {
-  Box,
-  Button,
-  Flex,
-  Input,
-  Stack,
-  Spinner,
-  Text,
-  useDisclosure
-} from "@chakra-ui/react";
-import {DadosTeste} from './data/dados'; // Importa os dados do banco de dados
-import React, { useEffect, useState } from 'react';
-import jsPDF from 'jspdf'; // Importa a biblioteca jsPDF para geração de PDF
+import { useState, useEffect } from 'react';
+import { Box, Button, Flex, Input, Stack, Spinner, Text } from "@chakra-ui/react";
+import { DadosTeste } from './data/dados'; // Importa os dados do banco de dados
+import jsPDF from 'jspdf';// Importa a biblioteca jsPDF para geração de PDF
 
 export default function Home() {
-   // Estado para armazenar o arquivo selecionado
-  const [selectedFile, setSelectedFile] = useState<File | null | any>(null); 
-  // Estado para armazenar o resultado da pesquisa
-  const [searchResult, setSearchResult] = useState(''); 
-  // Estado para armazenar os números de série não encontrados
-  const [notFoundSerials, setNotFoundSerials] = useState<string[]>([]); 
-  // Estado para controlar o spinner de carregamento
-  const [loading, setLoading] = useState(false); 
-   // Estado para controlar a visibilidade do botão "Voltar ao topo"
+     // Estado para armazenar o arquivo selecionado
+  const [selectedFile, setSelectedFile] = useState(null);
+    // Estado para armazenar o resultado da pesquisa
+  const [searchResult, setSearchResult] = useState('');
+    // Estado para armazenar os números de série não encontrados
+  const [notFoundSerials, setNotFoundSerials] = useState([]);
+    // Estado para controlar o spinner de carregamento
+  const [loading, setLoading] = useState(false);
+     // Estado para controlar a visibilidade do botão "Voltar ao topo"
   const [showBackToTop, setShowBackToTop] = useState(false);
 
-
-  /* Este efeito é usado para adicionar um event listener ao scroll da janela 
+    /* Este efeito é usado para adicionar um event listener ao scroll da janela 
   ** quando o componente é montado.
   ** Ele chama a função handleScroll sempre que o evento de scroll ocorre.
   ** O array vazio [] no final significa que este
   ** efeito será executado apenas uma vez, após o componente ser montado.*/
-useEffect(() => {
-  window.addEventListener('scroll', handleScroll);
-  // Retorna uma função de limpeza para remover o event listener quando o componente for desmontado.
-  return () => window.removeEventListener('scroll', handleScroll);
-}, []);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setShowBackToTop(window.scrollY > 300);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+     // Retorna uma função de limpeza para remover o event listener quando o componente for desmontado.
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  /* Esta função é chamada quando o usuário clica no botão "Voltar para o topo".
+** Ela utiliza o método scrollTo para rolar a janela para o topo da página,
+** com um efeito de rolagem suave definido pela opção behavior: "smooth".*/
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  
 /* Esta função é chamada quando ocorre um evento de scroll.
 ** Ela verifica a posição do scroll da janela (window.scrollY) e, se for maior que 300 pixels,
 ** define o estado setShowBackToTop como true para mostrar o botão de voltar para o topo.
 ** Caso contrário, define o estado como false para esconder o botão.*/
-const handleScroll = () => {
-  if (window.scrollY > 300) {
-    setShowBackToTop(true);
-  } else {
-    setShowBackToTop(false);
-  }
-};
-
-/* Esta função é chamada quando o usuário clica no botão "Voltar para o topo".
-** Ela utiliza o método scrollTo para rolar a janela para o topo da página,
-** com um efeito de rolagem suave definido pela opção behavior: "smooth".*/
-const scrollToTop = () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-};
-
 
   // Função para lidar com a mudança no arquivo selecionado
-  const handleFileChange = (event: any) => {
-    const file = event.target.files[0];
-    setSelectedFile(file);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files?.[0];
+    setSelectedFile(file || null);
   };
-
   // Função para processar o upload do arquivo
+
   const handleUpload = () => {
-    if (selectedFile) {
-      setLoading(true); // Ativa o spinner de carregamento
+    if (!selectedFile) {
+      alert("Nenhum arquivo selecionado.");
+      return;
+    }
 
+    setLoading(true);// Ativa o spinner de carregamento
+    const reader = new FileReader();
+    reader.onload = () => {
+      const fileContent = reader.result;
+      const notFoundSerialsTemp = [];
 
-      const reader = new FileReader();
-      reader.onload = () => {
-        const fileContent = reader.result as string;
-
-        const notFoundSerialsTemp: string[] = [];
-
-       /*estamos dividindo o conteúdo do arquivo .txt em linhas usando split('\n'),
+        /*estamos dividindo o conteúdo do arquivo .txt em linhas usando split('\n'),
        ** depois percorrendo cada número de série no arquivo. Para cada número de 
        ** série no arquivo, verificamos se ele está presente no banco de dados usando 
        ** DadosTeste.some(). Se não estiver presente no banco de dados, adicionamos à 
        ** lista notFoundSerials.*/ 
-        const serialsInFile = fileContent.split('\n');
-        serialsInFile.forEach(serial => {
-          // Verifica se o número de série não está presente no banco de dados
-          const isSerialPresentInDB = DadosTeste.some(item => item.serial === serial.trim());
-          if (!isSerialPresentInDB) {
-            notFoundSerialsTemp.push(serial.trim());
-          }
-        });
+      const serialsInFile = fileContent.split('\n');
+      serialsInFile.forEach(serial => {
+        // Verifica se o número de série não está presente no banco de dados
 
-        // Verifica se há números de série não encontrados
-        if (notFoundSerialsTemp.length === 0) {
-          setSearchResult(
-            'Todos os números de série do arquivo estão presentes no banco de dados.'
-            );
-            console.log(`
-            Número de série ${notFoundSerialsTemp}
-             encontrado no arquivo.${selectedFile}
-             `);
-        } else {
-          setSearchResult('');
-          setNotFoundSerials(notFoundSerialsTemp); // Atualiza o estado com os números de série não encontrados
+        if (!DadosTeste.some(item => item.serial === serial.trim())) {
+          notFoundSerialsTemp.push(serial.trim());
         }
-
-        setLoading(false); // Desativa o spinner de carregamento
-      };
-
-      reader.readAsText(selectedFile);
-    } else {
-      alert("Nenhum arquivo selecionado.");
-    }
-  };
-
-  // Função para lidar com o download do PDF
-  const handleDownload = () => {
-    if (selectedFile) {
-      const doc = new jsPDF(); // Cria um novo documento PDF
-      doc.text(searchResult, 10, 10); // Adiciona o resultado da pesquisa ao PDF
-      doc.text('Números de série não encontrados:', 10, 20); // Adiciona um título para os números de série não encontrados
-
-      // Itera sobre os números de série não encontrados e os adiciona ao PDF
-      notFoundSerials.forEach((serial, index) => {
-        doc.text(`${index + 1}. ${serial}`, 10, 30 + (index * 10));
       });
 
-      doc.save('serial_numbers.pdf'); // Salva o PDF com o nome especificado
-    } else {
+      // Verifica se há números de série não encontrados
+
+      if (notFoundSerialsTemp.length === 0) {
+        setSearchResult('Todos os números de série do arquivo estão presentes no banco de dados.');
+      } else {
+        setSearchResult('');
+        setNotFoundSerials(notFoundSerialsTemp);// Atualiza o estado com os números de série não encontrados
+      }
+
+      setLoading(false);// Desativa o spinner de carregamento
+
+    };
+
+    reader.readAsText(selectedFile);
+  };
+  // Função para lidar com o download do PDF
+
+  const handleDownload = () => {
+    if (!selectedFile) {
       alert("Nenhum arquivo para download.");
+      return;
     }
+
+    const doc = new jsPDF();// Cria um novo documento PDF
+    let pageNumber = 1;
+    let yPosition = 10;
+
+    doc.setFontSize(11);
+    doc.text(searchResult, 10, yPosition); // Adiciona o resultado da pesquisa ao PDF
+    yPosition += 10;
+
+    doc.text('Números de série não encontrados:', 10, yPosition); // Adiciona um título para os números de série não encontrados
+    yPosition += 10;
+
+    // Itera sobre os números de série não encontrados e os adiciona ao PDF
+
+    notFoundSerials.forEach((serial, index) => {
+      if (yPosition > 280) {
+        doc.addPage();
+        pageNumber++;
+        yPosition = 10;
+        doc.setFontSize(12);
+        doc.text(`Página ${pageNumber}`, 10, yPosition);
+        yPosition += 10;
+      }
+      doc.text(`${index + 1}. ${serial}`, 10, yPosition);
+      yPosition += 10;
+    });
+
+    doc.save('serial_numbers.pdf');// Salva o PDF com o nome especificado
   };
 
-  // Função para limpar a lista de números de série não encontrados
+    // Função para limpar a lista de números de série não encontrados
+
   const clearNotFoundSerials = () => {
     setNotFoundSerials([]);
   };
 
   return (
-    <>
+    < >
       <Flex
-        flexDirection={'column'}
+       bg={'black.50'}
+       flexDirection={'column'}
         justify={'center'}
         px={4}
       >
@@ -166,7 +166,7 @@ const scrollToTop = () => {
           <Button colorScheme='teal' variant='outline' onClick={handleDownload}>
             Download PDF
           </Button>
-          {notFoundSerials.length > 0 && ( // Renderiza o botão apenas se houver números de série não encontrados
+          {notFoundSerials.length > 0 && (
             <Button colorScheme='red' variant='outline' onClick={clearNotFoundSerials}>
               Limpar lista
             </Button>
