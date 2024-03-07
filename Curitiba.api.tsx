f622f1e3199e2615925e7c2b41a8caabe4871329
Gabriel Matos <120828640+Gabriel-Matos1@users.noreply.github.com>
2024-03-07 11:42:49 -0300

    Add files via upload

---
+++ Curitiba.api.tsx
@@ -0,0 +1,18 @@
+
+const Sequelize = require('sequelize');
+
+const DBC = new Sequelize('bancos_radios', 'postgres', '123456', {
+  host: 'localhost',
+  dialect: 'postgres'
+});
+
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Conexão com o banco de dados estabelecida com sucesso.');
+  })
+  .catch((err: any) => {
+    console.error('Erro ao conectar-se ao banco de dados:', err);
+  });
+module.exports = DBC; 
